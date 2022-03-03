fa5718c7cb9b7a3a96681ca3b0ce412287508702
ManifoldFR <wilson.jallet@polytechnique.org>
2022-03-03 22:58:39 +0100

    Base manifold

---
+++ src/manifold-base.hpp
@@ -0,0 +1,14 @@
+
+
+namespace lienlp {
+
+template<typename Scalar>
+class Manifold;
+
+/// N-dimensional vector space.
+template<typename Scalar, unsigned int N>
+class VectorSpace : public Manifold<Scalar> {};
+
+
+}
+
