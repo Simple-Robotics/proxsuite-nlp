339ce575f1e7212472993446ed7fef0385712db0
ManifoldFR <wilson.jallet@polytechnique.org>
2022-03-04 16:04:46 +0100

    add unit test, working config

---
+++ unittest/manifold.cpp
@@ -0,0 +1,15 @@
+#include "lienlp/manifold-base.hpp"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/utility/binary.hpp>
+
+
+BOOST_AUTO_TEST_SUITE( manifold )
+
+BOOST_AUTO_TEST_CASE( test_vector )
+{
+  lienlp::VectorSpace<double, 40> space;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
