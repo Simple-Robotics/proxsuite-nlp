156402528e3591f6c3cd274cb4c3d6140a5be2f8
ManifoldFR <wilson.jallet@polytechnique.org>
2022-03-15 19:12:12 +0100

    Quadratic residuals (for constrained-norm problems!)

---
+++ src/modelling/constraints/quadratic-residual.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+
+#include "lienlp/manifold-base.hpp"
+#include "lienlp/residual-base.hpp"
+
+
+namespace lienlp {
+  template<typename M>
+  struct QuadraticResidual : ResidualBase<typename M::Scalar>
+  {
+    using Scalar = typename M::Scalar;
+    LIENLP_RESIDUAL_TYPES(Scalar)
+    LIENLP_DEFINE_DYNAMIC_TYPES(Scalar)
+    using Base = ResidualBase<Scalar>;
+    using Base::computeJacobian;
+    using Base::m_ndx;
+    using Base::m_nr;
+
+
+    const M& m_manifold;
+    const MatrixXs m_weights;
+    const Scalar m_level;
+    const VectorXs m_target;
+
+    QuadraticResidual(const M& manifold,
+                      const MatrixXs& Q,
+                      const Scalar level,
+                      const VectorXs& target)
+                      : Base(manifold.nx(), manifold.ndx(), 1),
+                        m_manifold(manifold), 
+                        m_weights(Q),
+                        m_level(level),
+                        m_target(target) {}
+
+    QuadraticResidual(const M& manifold,
+                      const Scalar level,
+                      const VectorXs& target)
+      : QuadraticResidual(manifold,
+                          MatrixXs::Identity(manifold.ndx(), manifold.ndx()),
+                          level,
+                          target) {}
+
+    ReturnType operator()(const ConstVectorRef& x) const
+    {
+      VectorXs err = m_manifold.difference(m_target, x);
+      ReturnType ret(1, 1);
+      ret(0, 0) = err.dot(m_weights * err) - m_level;
+      return ret;
+    }
+
+    void computeJacobian(const ConstVectorRef& x, Eigen::Ref<JacobianType> Jout) const
+    {
+      VectorXs err = m_manifold.difference(m_target, x);
+      MatrixXs Jerr(m_manifold.ndx(), m_manifold.ndx());
+      m_manifold.Jdifference(m_target, x, Jerr, 1);
+      Jout = (err.transpose() * m_weights) * Jerr;
+    }
+
+  };
+} // namespace lienlp
+
