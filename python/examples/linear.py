a6fd932f5d71c071184db66e5c1ffa168eef058f
ManifoldFR <wilson.jallet@polytechnique.org>
2022-03-24 20:05:24 +0100

    python/examples: test negative orthant

--- python/examples/linear.py
+++ python/examples/linear.py
@@ -1,19 +1,31 @@
 import numpy as np
-from lienlp import LinearResidual, EqualityConstraint
+from lienlp import LinearResidual, EqualityConstraint, NegativeOrthant, Problem
 
-A = np.random.randn(2, 2)
+A = np.random.randn(2, 3)
 b = np.random.randn(2)
-x0 = np.linalg.solve(A, -b)
+x0 = np.linalg.lstsq(A, -b)[0]
+x1 = np.linalg.lstsq(A, np.abs(b) * .1)[0]
 
 resdl = LinearResidual(A, b)
 
+print("x0:", x0, "resdl(x0):", resdl(x0))
+print("x1:", x1, "resdl(x1):", resdl(x1))
 assert np.allclose(resdl.computeJacobian(x0), A)
 assert np.allclose(resdl(x0), 0.)
-assert np.allclose(resdl(np.zeros_like(b)), b)
+assert np.allclose(resdl(np.zeros_like(x0)), b)
 
-print("x0:", x0)
+print("Residual nx :", resdl.nx)
+print("Residual ndx:", resdl.ndx)
+print("Residual nr :", resdl.nr)
 
-cstr = EqualityConstraint(resdl)
 
-print(cstr.projection(x0), "should be zero")
-print(cstr.normalConeProjection(x0), "should be x0")
+cstr1 = EqualityConstraint(resdl)
+cstr2 = NegativeOrthant(resdl)
+
+print(cstr1.projection(x0), "should be zero")
+print(cstr1.normalConeProjection(x0), "should be x0")
+
+print("proj  x0:", cstr2.projection(x0))
+print("proj  x1:", cstr2.projection(x1))
+print("dproj x1:", cstr2.normalConeProjection(x1))
+
