713183a9ebe0214b53e3d09e687b0fa7f964264f
ManifoldFR <wilson.jallet@polytechnique.org>
2022-04-08 23:13:50 +0200

    expose some workspace fields

--- python/expose-workspace.cpp
+++ python/expose-workspace.cpp
@@ -13,7 +13,10 @@ namespace lienlp
       bp::class_<context::Workspace_t>(
         "Workspace", "SolverTpl workspace.",
         bp::init<int, int, const context::Problem_t&>(bp::args("nx", "ndx", "problem"))
-      );
+      )
+        .def_readonly("kkt_matrix", &context::Workspace_t::kktMatrix, "KKT matrix buffer.")
+        .def_readonly("kkt_rhs", &context::Workspace_t::kktRhs, "KKT system right-hand side buffer.")
+      ;
     }
     
   } // namespace python
