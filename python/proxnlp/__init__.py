b34ae3d3e357d8f971cd58c9ff8fcd222c0cda31
ManifoldFR <wilson.jallet@polytechnique.org>
2022-04-27 11:14:07 +0200

    python: cleanup __init__ file

--- python/proxnlp/__init__.py
+++ python/proxnlp/__init__.py
@@ -1,11 +1,17 @@
 from .pyproxnlp import *
 from .pyproxnlp import __version__
 
-import sys
-import inspect
 
-lib_name = 'proxnlp'
+def __process():
+    import sys
+    import inspect
+    from . import pyproxnlp
 
-submodules = inspect.getmembers(pyproxnlp, inspect.ismodule)
-for mod_info in submodules:
-    sys.modules["{}.{}".format(lib_name, mod_info[0])] = mod_info[1]
+    lib_name = 'proxnlp'
+
+    submodules = inspect.getmembers(pyproxnlp, inspect.ismodule)
+    for mod_info in submodules:
+        sys.modules["{}.{}".format(lib_name, mod_info[0])] = mod_info[1]
+
+
+__process()
