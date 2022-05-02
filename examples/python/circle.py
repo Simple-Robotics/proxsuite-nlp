46bbab51d06c961dae37c65e1d7fa7d8756dd7ff
ManifoldFR <wilson.jallet@polytechnique.org>
2022-05-02 17:45:31 +0200

    examples/python/circle: add assert for number of iters

--- examples/python/circle.py
+++ examples/python/circle.py
@@ -76,56 +76,58 @@ solver.solve(workspace, results, p1, lams0)
 print("Result x:  ", results.xopt)
 print("Target was:", p0)
 
-
-plt.rcParams["lines.linewidth"] = 1.
-
-xs_ = np.stack(callback.storage.xs.tolist())
-
-bound_xs = (np.min(xs_[:, 0]), np.max(xs_[:, 0]),
-            np.min(xs_[:, 1]), np.max(xs_[:, 1]))
-# left,right,bottom,up
-
-ax: plt.Axes = plt.axes()
-ax.plot(*xs_.T, marker='.', markersize=2, alpha=.7, color='b')
-ax.scatter(*xs_[-1], s=12, c='tab:red', marker='o', label="$x^*$", zorder=2)
-circ_alpha = 0.3
-ar_c1 = plt.Circle(center1, radius, facecolor='r', alpha=circ_alpha, edgecolor='k')
-ar_c2 = plt.Circle(center2, radius, facecolor='b', alpha=circ_alpha, edgecolor='k')
-ar_c3 = plt.Circle(center3, radius, facecolor='g', alpha=circ_alpha, edgecolor='k')
-
-ax.add_patch(ar_c1)
-ax.add_patch(ar_c2)
-ax.add_patch(ar_c3)
-ax.set_aspect('equal')
-
-ax.scatter(*p0, c='green', marker='o')
-ax.text(*p0, "$p_0$")
-
-xlims = ax.get_xlim()
-xlims = (min(bound_xs[0], xlims[0]), max(bound_xs[1], xlims[1]))
-ylims = ax.get_ylim()
-ylims = (min(bound_xs[2], ylims[0]), max(bound_xs[3], ylims[1]))
-ax.set_xlim(*xlims)
-ax.set_ylim(*ylims)
-plt.legend()
-plt.title("Optimization trajectory")
+assert results.numiters == 17  # numiters as of ef27e38
 
 
-it_list = [1, 2, 3, 4, 5, 10, 20, 30]
-it_list = [i for i in it_list if i < results.numiters]
-for it in it_list:
-    ls_alphas = callback.storage.ls_alphas[it].copy()
-    ls_values = callback.storage.ls_values[it].copy()
-    if len(ls_alphas) == 0:
-        continue
-    soidx = np.argsort(ls_alphas)
-    ls_alphas = ls_alphas[soidx]
-    ls_values = ls_values[soidx]
-    d1 = callback.storage.d1_s[it]
-    plt.figure()
-    plt.plot(ls_alphas, ls_values)
-    plt.plot(ls_alphas, ls_values[0] + ls_alphas * d1)
-    plt.plot(ls_alphas, ls_values[0] + solver.armijo_c1 * ls_alphas * d1, ls='--')
-    plt.title("Iteration %d" % it)
+if __name__ == '__main__':
+    plt.rcParams["lines.linewidth"] = 1.
 
-plt.show()
+    xs_ = np.stack(callback.storage.xs.tolist())
+
+    bound_xs = (np.min(xs_[:, 0]), np.max(xs_[:, 0]),
+                np.min(xs_[:, 1]), np.max(xs_[:, 1]))
+    # left,right,bottom,up
+
+    ax: plt.Axes = plt.axes()
+    ax.plot(*xs_.T, marker='.', markersize=2, alpha=.7, color='b')
+    ax.scatter(*xs_[-1], s=12, c='tab:red', marker='o', label="$x^*$", zorder=2)
+    circ_alpha = 0.3
+    ar_c1 = plt.Circle(center1, radius, facecolor='r', alpha=circ_alpha, edgecolor='k')
+    ar_c2 = plt.Circle(center2, radius, facecolor='b', alpha=circ_alpha, edgecolor='k')
+    ar_c3 = plt.Circle(center3, radius, facecolor='g', alpha=circ_alpha, edgecolor='k')
+
+    ax.add_patch(ar_c1)
+    ax.add_patch(ar_c2)
+    ax.add_patch(ar_c3)
+    ax.set_aspect('equal')
+
+    ax.scatter(*p0, c='green', marker='o')
+    ax.text(*p0, "$p_0$")
+
+    xlims = ax.get_xlim()
+    xlims = (min(bound_xs[0], xlims[0]), max(bound_xs[1], xlims[1]))
+    ylims = ax.get_ylim()
+    ylims = (min(bound_xs[2], ylims[0]), max(bound_xs[3], ylims[1]))
+    ax.set_xlim(*xlims)
+    ax.set_ylim(*ylims)
+    plt.legend()
+    plt.title("Optimization trajectory")
+
+    it_list = [1, 2, 3, 4, 5, 10, 20, 30]
+    it_list = [i for i in it_list if i < results.numiters]
+    for it in it_list:
+        ls_alphas = callback.storage.ls_alphas[it].copy()
+        ls_values = callback.storage.ls_values[it].copy()
+        if len(ls_alphas) == 0:
+            continue
+        soidx = np.argsort(ls_alphas)
+        ls_alphas = ls_alphas[soidx]
+        ls_values = ls_values[soidx]
+        d1 = callback.storage.d1_s[it]
+        plt.figure()
+        plt.plot(ls_alphas, ls_values)
+        plt.plot(ls_alphas, ls_values[0] + ls_alphas * d1)
+        plt.plot(ls_alphas, ls_values[0] + solver.armijo_c1 * ls_alphas * d1, ls='--')
+        plt.title("Iteration %d" % it)
+
+    plt.show()
