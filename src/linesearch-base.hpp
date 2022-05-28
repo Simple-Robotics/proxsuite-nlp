538969473533a83cafaec599bd7a1104574719c2
ManifoldFR <wilson.jallet@polytechnique.org>
2022-05-28 12:28:12 -0400

    linesearch: start work on quadratic

--- src/linesearch-base.hpp
+++ src/linesearch-base.hpp
@@ -8,6 +8,15 @@
 
 namespace proxnlp
 {
+
+  template<typename S>
+  S compute_merit_for_step(const SolverTpl<S>& solver, WorkspaceTpl<S>& workspace, const ResultsTpl<S>& results, const S a0)
+  {
+    SolverTpl<S>::tryStep(solver.manifold, workspace, results, a0);
+    S ret = solver.merit_fun(workspace.xTrial, workspace.lamsTrial, workspace.lamsPrev);
+    return ret + solver.prox_penalty.call(workspace.xTrial);
+  }
+
   /// @brief  Basic backtracking Armijo linesearch.
   template<typename _Scalar>
   struct ArmijoLinesearch
@@ -28,10 +37,9 @@ namespace proxnlp
       alpha_try = 1.;
       const Scalar ls_beta = solver.ls_beta;
       Scalar phi_trial = 0., dM = 0.;
-      const auto& manifold = solver.manifold;
       const Scalar armijo_c1 = solver.armijo_c1;
 
-      Solver::tryStep(manifold, workspace, results, alpha_try);
+      Solver::tryStep(solver.manifold, workspace, results, alpha_try);
 
       // exit if directional derivative is small
       if (std::abs(dphi0) < dphi_tresh) { return; }
@@ -42,15 +50,13 @@ namespace proxnlp
       do
       {
         phi_trial = solver.merit_fun(workspace.xTrial, workspace.lamsTrial, workspace.lamsPrev);
-        if (solver.rho > 0.) {
-          phi_trial += solver.prox_penalty.call(workspace.xTrial);
-        }
+        phi_trial += solver.prox_penalty.call(workspace.xTrial);
         dM = phi_trial - phi0;
 
         if (dM <= armijo_c1 * alpha_try * dphi0) { break; }
 
         alpha_try = std::max(alpha_try * ls_beta, solver.alpha_min);
-        Solver::tryStep(manifold, workspace, results, alpha_try);
+        Solver::tryStep(solver.manifold, workspace, results, alpha_try);
 
       } while (alpha_try > solver.alpha_min);
 
