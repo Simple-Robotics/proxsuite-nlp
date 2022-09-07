de9b4bde9b5ab59250dc234fc4d8c1c53b9ffd77
ManifoldFR <wilson.jallet@polytechnique.org>
2022-09-07 15:50:56 +0200

    python: move function wrapper classes to own header
    * add compose arg names

--- python/expose-function.cpp
+++ python/expose-function.cpp
@@ -1,81 +1,15 @@
-#include "proxnlp/python/fwd.hpp"
-#include "proxnlp/function-base.hpp"
+#include "proxnlp/python/function.hpp"
 #include "proxnlp/function-ops.hpp"
 
 namespace proxnlp {
 namespace python {
-namespace internal {
-struct FunctionWrap : context::Function, bp::wrapper<context::Function> {
-public:
-  PROXNLP_FUNCTION_TYPEDEFS(context::Scalar);
-
-  FunctionWrap(const int nx, const int ndx, const int nr)
-      : context::Function(nx, ndx, nr) {}
-
-  ReturnType operator()(const ConstVectorRef &x) const {
-    bp::override f = get_override("__call__");
-    return f(x);
-  }
-};
-
-struct C1FunctionWrap : context::C1Function, bp::wrapper<context::C1Function> {
-  PROXNLP_FUNCTION_TYPEDEFS(context::Scalar);
-
-  C1FunctionWrap(const int nx, const int ndx, const int nr)
-      : context::C1Function(nx, ndx, nr) {}
-
-  ReturnType operator()(const ConstVectorRef &x) const {
-    bp::override f = get_override("__call__");
-    return f(x);
-  }
-
-  void computeJacobian(const ConstVectorRef &x, MatrixRef Jout) const {
-    Jout.resize(this->nr(), this->ndx());
-    get_override("computeJacobian")(x, Jout);
-  }
-};
-
-struct C2FunctionWrap : context::C2Function, bp::wrapper<context::C2Function> {
-  PROXNLP_FUNCTION_TYPEDEFS(context::Scalar);
-
-  C2FunctionWrap(const int nx, const int ndx, const int nr)
-      : context::C2Function(nx, ndx, nr) {}
-
-  ReturnType operator()(const ConstVectorRef &x) const {
-    bp::override f = get_override("__call__");
-    return f(x);
-  }
-
-  void computeJacobian(const ConstVectorRef &x, MatrixRef Jout) const {
-    Jout.resize(this->nr(), this->ndx());
-    get_override("computeJacobian")(x, Jout);
-  }
-
-  void vectorHessianProduct(const ConstVectorRef &x, const ConstVectorRef &v,
-                            MatrixRef Hout) const {
-    Hout.resize(this->ndx(), this->ndx());
-    if (bp::override f = this->get_override("vectorHessianProduct")) {
-      f(x, v, Hout);
-      return;
-    } else {
-      return context::C2Function::vectorHessianProduct(x, v, Hout);
-    }
-  }
-
-  void default_vhp(const ConstVectorRef &x, const ConstVectorRef &v,
-                   MatrixRef Hout) const {
-    return context::C2Function::vectorHessianProduct(x, v, Hout);
-  }
-};
-
-} // namespace internal
 
 void exposeFunctionTypes() {
   using context::C1Function;
   using context::C2Function;
   using context::Function;
 
-  bp::class_<internal::FunctionWrap, boost::noncopyable>(
+  bp::class_<FunctionWrap, boost::noncopyable>(
       "BaseFunction", "Base class for functions.",
       bp::init<int, int, int>(bp::args("self", "nx", "ndx", "nr")))
       .def("__call__", bp::pure_virtual(&Function::operator()),
@@ -87,7 +21,7 @@ void exposeFunctionTypes() {
   context::MatFuncType C1Function::*compJac1 = &C1Function::computeJacobian;
   context::MatFuncRetType C1Function::*compJac2 = &C1Function::computeJacobian;
 
-  bp::class_<internal::C1FunctionWrap, bp::bases<Function>, boost::noncopyable>(
+  bp::class_<C1FunctionWrap, bp::bases<Function>, boost::noncopyable>(
       "C1Function", "Base class for differentiable functions",
       bp::init<int, int, int>())
       .def("computeJacobian", bp::pure_virtual(compJac1),
@@ -100,12 +34,12 @@ void exposeFunctionTypes() {
   context::VHPFuncRetType C2Function::*compHess2 =
       &C2Function::vectorHessianProduct;
 
-  bp::class_<internal::C2FunctionWrap, bp::bases<C1Function>,
+  bp::class_<C2FunctionWrap, bp::bases<C1Function>,
              boost::noncopyable>(
       "C2Function", "Base class for twice-differentiable functions.",
       bp::init<int, int, int>())
       .def("vectorHessianProduct", compHess1,
-           &internal::C2FunctionWrap::default_vhp,
+           &C2FunctionWrap::default_vhp,
            bp::args("self", "x", "v", "Hout"))
       .def("get_vhp", compHess2, bp::args("self", "x", "v"),
            "Compute and return the vector-Hessian product.");
@@ -116,6 +50,7 @@ void exposeFunctionTypes() {
           bp::args("self", "left", "right")));
 
   bp::def("compose", &::proxnlp::compose<context::Scalar>,
+          bp::args("fn1", "fn2"),
           "Returns the composition of two C2Function objects.");
 }
 
---
+++ python/function.hpp
@@ -0,0 +1,73 @@
+#include "proxnlp/python/fwd.hpp"
+
+#include "proxnlp/function-base.hpp"
+
+
+namespace proxnlp::python
+{
+  
+struct FunctionWrap : context::Function, bp::wrapper<context::Function> {
+public:
+  PROXNLP_FUNCTION_TYPEDEFS(context::Scalar);
+
+  FunctionWrap(const int nx, const int ndx, const int nr)
+      : context::Function(nx, ndx, nr) {}
+
+  ReturnType operator()(const ConstVectorRef &x) const {
+    bp::override f = get_override("__call__");
+    return f(x);
+  }
+};
+
+struct C1FunctionWrap : context::C1Function, bp::wrapper<context::C1Function> {
+  PROXNLP_FUNCTION_TYPEDEFS(context::Scalar);
+
+  C1FunctionWrap(const int nx, const int ndx, const int nr)
+      : context::C1Function(nx, ndx, nr) {}
+
+  ReturnType operator()(const ConstVectorRef &x) const {
+    bp::override f = get_override("__call__");
+    return f(x);
+  }
+
+  void computeJacobian(const ConstVectorRef &x, MatrixRef Jout) const {
+    Jout.resize(this->nr(), this->ndx());
+    get_override("computeJacobian")(x, Jout);
+  }
+};
+
+struct C2FunctionWrap : context::C2Function, bp::wrapper<context::C2Function> {
+  PROXNLP_FUNCTION_TYPEDEFS(context::Scalar);
+
+  C2FunctionWrap(const int nx, const int ndx, const int nr)
+      : context::C2Function(nx, ndx, nr) {}
+
+  ReturnType operator()(const ConstVectorRef &x) const {
+    bp::override f = get_override("__call__");
+    return f(x);
+  }
+
+  void computeJacobian(const ConstVectorRef &x, MatrixRef Jout) const {
+    Jout.resize(this->nr(), this->ndx());
+    get_override("computeJacobian")(x, Jout);
+  }
+
+  void vectorHessianProduct(const ConstVectorRef &x, const ConstVectorRef &v,
+                            MatrixRef Hout) const {
+    Hout.resize(this->ndx(), this->ndx());
+    if (bp::override f = this->get_override("vectorHessianProduct")) {
+      f(x, v, Hout);
+      return;
+    } else {
+      return context::C2Function::vectorHessianProduct(x, v, Hout);
+    }
+  }
+
+  void default_vhp(const ConstVectorRef &x, const ConstVectorRef &v,
+                   MatrixRef Hout) const {
+    return context::C2Function::vectorHessianProduct(x, v, Hout);
+  }
+};
+
+} // namespace proxnlp::python
+
