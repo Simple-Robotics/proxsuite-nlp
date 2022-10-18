ea203cc058fd8a9760f104812cc61e04680d0122
ManifoldFR <wilson.jallet@polytechnique.org>
2022-10-18 13:33:36 +0200

    update examples/ur_ik

--- examples/ur_ik.py
+++ examples/ur_ik.py
@@ -1,5 +1,8 @@
+"""
+Inverse kinematics with box constraints.
+"""
 import proxnlp
-from proxnlp import manifolds, costs
+from proxnlp import manifolds, costs, constraints
 from proxnlp import C2Function
 
 import numpy as np
@@ -16,11 +19,11 @@ class FramePlacement(C2Function):
         self.model = model
         self.data: pin.Data = model.createData()
         self.fid = fid
-        self.M_ref = M_ref
+        self.M_ref = M_ref.copy()
 
     def __call__(self, x):
         pin.forwardKinematics(self.model, self.data, x)
-        M = self.data.oMi[self.fid]
+        M = self.data.oMi[self.fid].copy()
         return pin.log6(self.M_ref.actInv(M)).vector
 
     def computeJacobian(self, x, Jout):
@@ -31,7 +34,7 @@ class FramePlacement(C2Function):
         )
 
 
-robot = erd.load("ur5")
+robot = erd.load("panda")
 model = robot.model
 rdata = robot.data
 
@@ -51,14 +54,13 @@ space = manifolds.MultibodyConfiguration(model)
 weights = np.eye(space.ndx)
 
 q_ref = pin.neutral(model)
-q_ref = pin.integrate(model, q_rand, 4.0 * np.random.rand(model.nv))
-q_ref = pin.integrate(model, q_rand, 4.0 * np.ones(model.nv))
+q_ref = pin.integrate(model, q_rand, 1.0 * np.ones(model.nv))
 cost = costs.QuadraticDistanceCost(space, q_ref, weights)
 
 model.lowerPositionLimit[:] = -1e2
 model.upperPositionLimit[:] = +1e2
 
-ee_name = model.names[-1]
+ee_name = model.names[-3]
 ee_id = model.getJointId(ee_name)
 pin.forwardKinematics(model, rdata, q_rand)
 M_ref = rdata.oMi[ee_id].copy()
@@ -82,15 +84,18 @@ for _ in range(10):
     test_fd()
 
 
-constraint = proxnlp.constraints.createEqualityConstraint(frame_pl_autodiff)
-
-problem = proxnlp.Problem(space, cost, [constraint])
+cstr1 = constraints.createEqualityConstraint(frame_pl_autodiff)
+fun2 = proxnlp.residuals.LinearFunction(np.eye(space.ndx))
+cstr2 = constraints.ConstraintObject(
+    fun2, constraints.BoxConstraint(model.lowerPositionLimit, model.upperPositionLimit)
+)
+problem = proxnlp.Problem(space, cost, [cstr1, cstr2])
 
 tol = 1e-3
-mu_init = 0.01
-rho_init = 1e-6
+mu_init = 1e-4
+rho_init = 0.0
 solver = proxnlp.Solver(problem, tol, mu_init, rho_init, verbose=proxnlp.VERBOSE)
-
+solver.mul_update_mode = proxnlp.MUL_PRIMAL_DUAL
 
 rs = proxnlp.Results(problem)
 ws = proxnlp.Workspace(problem)
@@ -98,3 +103,13 @@ solver.register_callback(proxnlp.helpers.HistoryCallback())
 solver.solve(ws, rs, q_ref)
 
 print(rs)
+
+
+print(rs.xopt)
+print("dist to qref:  ", pin.distance(model, rs.xopt, q_ref))
+print("dist to qrand: ", pin.distance(model, rs.xopt, q_rand))
+print("|qref- qrand|: ", pin.distance(model, q_ref, q_rand))
+
+vizer.viewer.open()
+input()
+vizer.display(rs.xopt)
