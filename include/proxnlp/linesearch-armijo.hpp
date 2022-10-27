0afd608f7e5a9750ed0d4730e941ac8e08a7c49a
ManifoldFR <wilson.jallet@polytechnique.org>
2022-10-27 19:08:48 +0200

    linesearch-armijo: add more safeguards
    against NaNs, failures in evaluating the function, etc

--- include/proxnlp/linesearch-armijo.hpp
+++ include/proxnlp/linesearch-armijo.hpp
@@ -46,23 +46,35 @@ public:
   using VectorXs = typename math_types<Scalar>::VectorXs;
   using Base::options;
 
+  Polynomial interpol;
+
   ArmijoLinesearch(const typename Base::Options &options)
       : Base(options), lu(alph_mat) {}
 
   template <typename Fn>
   Scalar run(Fn phi, const Scalar phi0, const Scalar dphi0, Scalar &alpha_try) {
-    auto eval_sample_nograd = [&](const Scalar a) {
-      return FunctionSample{a, phi(a)};
-    };
-
-    const FunctionSample lowerbound(0., phi0, dphi0);
+    const FunctionSample lower_bound(0., phi0, dphi0);
 
     alpha_try = 1.;
-    FunctionSample latest = eval_sample_nograd(alpha_try);
+    FunctionSample latest;
     FunctionSample previous;
 
+    // try the full step; if failure encountered, aggressively
+    // backtrack until no exception is raised
+    while (true) {
+      try {
+        latest = FunctionSample(alpha_try, phi(alpha_try));
+        break;
+      } catch (const std::runtime_error &e) {
+        alpha_try *= 0.5;
+        if (alpha_try <= options().alpha_min) {
+          alpha_try = options().alpha_min;
+        }
+      }
+    }
+
     if (std::abs(dphi0) < options().dphi_thresh) {
-      return phi0;
+      return alpha_try;
     }
 
     for (std::size_t i = 0; i < options().max_num_steps; i++) {
@@ -72,24 +84,22 @@ public:
         break;
       }
 
+      // compute next alpha try
       LSInterpolation strat = options().interp_type;
       if (strat == LSInterpolation::BISECTION) {
         alpha_try *= 0.5;
       } else {
-        std::vector<FunctionSample> samples;
-        samples.push_back(lowerbound);
+        std::vector<FunctionSample> samples = {lower_bound};
 
         // interpolation routines
         switch (strat) {
         case LSInterpolation::QUADRATIC: {
-          // two point interpolation: value, derivative at 0
-          // and latest value
+          // 2-point interp: value, derivative at 0 and latest value
           samples.push_back(latest);
           break;
         }
         case LSInterpolation::CUBIC: {
-          // three point interpolation: phi(0), phi'(0)
-          // and last two values
+          // 3-point interp: phi(0), phi'(0) and last two values
           samples.push_back(latest);
           if (previous.valid) {
             samples.push_back(previous);
@@ -106,9 +116,21 @@ public:
             strat, samples, options().contraction_min * alpha_try,
             options().contraction_max * alpha_try);
       }
-      alpha_try = std::max(alpha_try, options().alpha_min);
-      previous = latest;
-      latest = eval_sample_nograd(alpha_try);
+
+      if (std::isnan(alpha_try)) {
+        // handle NaN case
+        alpha_try = options().contraction_min * previous.alpha;
+      } else {
+        alpha_try = std::max(alpha_try, options().alpha_min);
+      }
+
+      try {
+        previous = latest;
+        latest = FunctionSample(alpha_try, phi(alpha_try));
+      } catch (const std::runtime_error &e) {
+        continue;
+      }
+
       if (alpha_try <= options().alpha_min) {
         break;
       }
@@ -128,13 +150,12 @@ public:
                               const std::vector<FunctionSample> &samples,
                               Scalar min_step_size, Scalar max_step_size) {
     Scalar anext = 0.0;
-    Polynomial interpol;
     VectorXs &coeffs = interpol.coeffs;
 
     assert(samples.size() >= 2);
-    const FunctionSample &lowerbound = samples[0];
-    const Scalar &phi0 = lowerbound.phi;
-    const Scalar &dphi0 = lowerbound.dphi;
+    const FunctionSample &lower_bound = samples[0];
+    const Scalar &phi0 = lower_bound.phi;
+    const Scalar &dphi0 = lower_bound.dphi;
 
     if (samples.size() == 2) {
       strat = LSInterpolation::QUADRATIC;
@@ -148,7 +169,6 @@ public:
                  (cand0.alpha * cand0.alpha);
       coeffs.conservativeResize(3);
       coeffs << a, dphi0, phi0;
-      interpol = Polynomial(coeffs);
       assert(interpol.degree() == 2);
       anext = -dphi0 / (2. * a);
       break;
@@ -159,10 +179,6 @@ public:
       const FunctionSample &cand1 = samples[2];
       const Scalar &a0 = cand0.alpha;
       const Scalar &a1 = cand1.alpha;
-      // alph_mat(0, 0) = a0 * a0;
-      // alph_mat(0, 1) = -a1 * a1;
-      // alph_mat(1, 0) = -a0 * a0 * a0;
-      // alph_mat(1, 1) = a1 * a1 * a1;
       alph_mat(0, 0) = a0 * a0 * a0;
       alph_mat(0, 1) = a0 * a0;
       alph_mat(1, 0) = a1 * a1 * a1;
@@ -173,13 +189,11 @@ public:
 
       lu.compute(alph_mat);
       coeffs_cubic_interpolant = lu.solve(alph_rhs);
-      // coeffs_cubic_interpolant = alph_mat * alph_rhs;
 
       const Scalar c3 = coeffs_cubic_interpolant(0);
       const Scalar c2 = coeffs_cubic_interpolant(1);
       coeffs.conservativeResize(4);
       coeffs << c3, c2, dphi0, phi0;
-      interpol = Polynomial(coeffs);
       assert(interpol.degree() == 3);
 
       // minimizer of cubic interpolant -> solve dinterp/da = 0
@@ -194,7 +208,7 @@ public:
       // if min outside of (amin; amax), look at the edges
       Scalar pleft = interpol.evaluate(min_step_size);
       Scalar pright = interpol.evaluate(max_step_size);
-      if (pleft <= pright) {
+      if (pleft < pright) {
         anext = min_step_size;
       } else {
         anext = max_step_size;
