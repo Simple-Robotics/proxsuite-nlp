8de7fcadc43552726b3093ab8c4c59bbc8fd0237
ManifoldFR <wilson.jallet@polytechnique.org>
2022-10-31 13:15:12 +0100

    add openmp parallel

--- include/proxnlp/problem-base.hpp
+++ include/proxnlp/problem-base.hpp
@@ -70,6 +70,8 @@ public:
   void evaluate(const ConstVectorRef &x,
                 WorkspaceTpl<Scalar> &workspace) const {
     workspace.objective_value = cost().call(x);
+
+#pragma omp parallel for
     for (std::size_t i = 0; i < getNumConstraints(); i++) {
       const ConstraintType &cstr = constraints_[i];
       workspace.cstr_values[i] = cstr.func()(x);
@@ -79,6 +81,8 @@ public:
   void computeDerivatives(const ConstVectorRef &x,
                           WorkspaceTpl<Scalar> &workspace) const {
     cost().computeGradient(x, workspace.objective_gradient);
+
+#pragma omp parallel for
     for (std::size_t i = 0; i < getNumConstraints(); i++) {
       const ConstraintType &cstr = constraints_[i];
       cstr.func().computeJacobian(x, workspace.cstr_jacobians[i]);
