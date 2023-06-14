ce88f3df4a31e451cfaba9b8fd250ec5276c5e6a
ManifoldFR <wilson.jallet@polytechnique.org>
2023-06-14 18:09:54 +0200

    [core] instantiate function template downcast_function_to_cost

--- src/cost-function.cpp
+++ src/cost-function.cpp
@@ -5,4 +5,7 @@ namespace proxnlp {
 
 template struct CostFunctionBaseTpl<context::Scalar>;
 
-}
+template auto downcast_function_to_cost<context::Scalar>(
+    const shared_ptr<context::C2Function> &) -> shared_ptr<context::Cost>;
+
+} // namespace proxnlp
