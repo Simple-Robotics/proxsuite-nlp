ad8fe512aa5e4edc3202be5f9646dcc9d67e5a9b
ManifoldFR <wilson.jallet@polytechnique.org>
2023-06-14 18:19:33 +0200

    [core] pdal: change multiline docstring style

--- include/proxnlp/pdal.hpp
+++ include/proxnlp/pdal.hpp
@@ -7,19 +7,18 @@
 
 namespace proxnlp {
 
-/**
- * @brief   Primal-dual augmented Lagrangian-type merit function.
- *
- * Primal-dual Augmented Lagrangian function, extending
- * the function from Gill & Robinson (2012) to inequality constraints.
- * For inequality constraints of the form \f$ c(x) \in \calC \f$ and an
- * objective function \f$ f\colon\calX \to \RR \f$, \f[ \calM_{\mu}(x, \lambda;
- * \lambda_e) = f(x) + \frac{1}{2\mu} \| \proj_\calC(c(x) + \mu \lambda_e)
- * \|_2^2
- *    + \frac{1}{2\mu} \| \proj_\calC(c(x) + \mu\lambda_e) - \mu\lambda) \|_2^2.
- * \f]
- *
- */
+///
+/// @brief   Primal-dual augmented Lagrangian-type merit function.
+///
+/// Primal-dual Augmented Lagrangian function, extending
+/// the function from Gill & Robinson (2012) to inequality constraints.
+/// For inequality constraints of the form \f$ c(x) \in \calC \f$ and an
+/// objective function \f$ f\colon\calX \to \RR \f$,
+/// \f[
+///  \calM_{\mu}(x, \lambda; \lambda_e) = f(x) + \frac{1}{2\mu}
+/// \dist(\proj_\calC(c(x) + \mu (\lambda_e - \lambda/2))^2 +
+/// \frac{\mu}{4}\|\lambda\|^2. \f]
+///
 template <typename _Scalar> struct ALMeritFunctionTpl {
 public:
   using Scalar = _Scalar;
