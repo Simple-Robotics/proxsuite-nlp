d967c57da0e1ce66cf303e0aace01584fbdbac5b
ManifoldFR <wilson.jallet@polytechnique.org>
2023-11-24 01:47:13 +0100

    Remove ldlt-allocator.cpp
    + change create_default_block_structure to be inline

--- include/proxnlp/ldlt-allocator.hpp
+++ include/proxnlp/ldlt-allocator.hpp
@@ -30,9 +30,54 @@ enum class LDLTChoice {
   PROXSUITE
 };
 
-SymbolicBlockMatrix
+inline SymbolicBlockMatrix
 create_default_block_structure(const std::vector<isize> &dims_primal,
-                               const std::vector<isize> &dims_dual);
+                               const std::vector<isize> &dims_dual) {
+
+  using linalg::BlockKind;
+
+  isize nprim_blocks = (isize)dims_primal.size();
+  isize ndual_blocks = (isize)dims_dual.size();
+  isize nblocks = nprim_blocks + ndual_blocks;
+
+  SymbolicBlockMatrix structure(nblocks, nblocks);
+  isize *segment_lens = structure.segment_lens;
+
+  for (unsigned int i = 0; i < nprim_blocks; ++i) {
+    segment_lens[i] = dims_primal[i];
+  }
+  for (unsigned int i = 0; i < ndual_blocks; ++i) {
+    segment_lens[i + nprim_blocks] = dims_dual[i];
+  }
+
+  // default structure: primal blocks are dense, others are sparse
+
+  for (isize i = 0; i < nprim_blocks; ++i) {
+    for (isize j = 0; j < nprim_blocks; ++j) {
+      structure(i, j) = linalg::Dense;
+    }
+  }
+
+  // jacobian blocks: assumed dense
+  for (isize i = 0; i < nprim_blocks; ++i) {
+    for (isize j = nprim_blocks; j < nblocks; ++j) {
+      structure(i, j) = linalg::Dense;
+      structure(j, i) = linalg::Dense;
+    }
+  }
+
+  for (isize i = nprim_blocks; i < nblocks; ++i) {
+    // diagonal blocks are diagonal
+    structure(i, i) = linalg::Diag;
+
+    // off-diagonal blocks are zero
+    for (isize j = nprim_blocks; j < nblocks; ++j) {
+      if (i != j)
+        structure(i, j) = linalg::Zero;
+    }
+  }
+  return structure;
+}
 
 inline isize get_total_dim_helper(const std::vector<isize> &nprims,
                                   const std::vector<isize> &nduals) {
