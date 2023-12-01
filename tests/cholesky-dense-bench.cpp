45a975b598b26738f91ad9efe749eec0eff53706
Joris Vaillant <joris.vaillant@inria.fr>
2023-12-01 17:56:13 +0100

    [tests] Add dense benchmark on positive semi definite matrices

--- tests/cholesky-dense-bench.cpp
+++ tests/cholesky-dense-bench.cpp
@@ -6,16 +6,34 @@
 #include "proxnlp/linalg/bunchkaufman.hpp"
 #include "proxnlp/linalg/dense.hpp"
 #include "util.hpp"
+
 #include <benchmark/benchmark.h>
 
-template <class DecType> void BM_posdef(benchmark::State &state) {
+template <class DecType> void BM_pos_def(benchmark::State &state) {
   Eigen::Rand::P8_mt19937_64 rng{42};
   typedef typename DecType::MatrixType MatrixType;
   for (auto _ : state) {
-    long n = state.range();
+    long n = state.range(0);
     state.PauseTiming();
     auto wgen = Eigen::Rand::makeWishartGen(n + 1, MatrixType::Identity(n, n));
     MatrixType a = wgen.generate(rng);
+    MatrixType b = Eigen::Rand::normal<MatrixType>(n, 4, rng);
+    state.ResumeTiming();
+
+    DecType dec(a);
+    dec.solveInPlace(b);
+  }
+}
+
+template <class DecType> void BM_pos_sem_def(benchmark::State &state) {
+  Eigen::Rand::P8_mt19937_64 rng{42};
+  typedef typename DecType::MatrixType MatrixType;
+  for (auto _ : state) {
+    long n = state.range(0);
+    state.PauseTiming();
+    MatrixType a_n =
+        Eigen::Rand::normal<MatrixType>(n, n - state.range(1), rng);
+    MatrixType a = a_n * a_n.transpose();
     MatrixType b = Eigen::Rand::normal<MatrixType>(n, 4, rng);
     state.ResumeTiming();
 
@@ -28,7 +46,7 @@ template <class DecType, typename MatrixType = typename DecType::MatrixType>
 void BM_indefinite(benchmark::State &state) {
   Eigen::Rand::P8_mt19937_64 rng{42};
   for (auto _ : state) {
-    long n = state.range();
+    long n = state.range(0);
     state.PauseTiming();
     MatrixType a = sampleGaussianOrhogonalEnsemble(n);
     MatrixType b = Eigen::Rand::normal<MatrixType>(n, 4, rng);
@@ -40,20 +58,32 @@ void BM_indefinite(benchmark::State &state) {
   }
 }
 
-const std::vector<long> dimArgs = {55,  64,  77,   115,  128,
-                                   256, 432, 1000, 2000, 4832};
+const std::vector<long> dimArgs = {55, 64, 77, 115, 128, 256, 432,
+                                   /*1000, 2000, 4832*/};
+const std::vector<long> nonDefArgs = {1, 2, 4, 8, 16, 32};
 static void custom_args(benchmark::internal::Benchmark *b) {
-
   b->ArgName("dim");
   for (auto d : dimArgs)
     b->Arg(d);
-  b->Unit(benchmark::kMicrosecond)->Repetitions(5)->UseRealTime();
+  b->Unit(benchmark::kMicrosecond)->Repetitions(1)->UseRealTime();
+}
+static void custom_args_pos_sem_def(benchmark::internal::Benchmark *b) {
+  b->ArgNames({"dim", "non def"});
+  b->ArgsProduct({dimArgs, nonDefArgs});
+  b->Unit(benchmark::kMicrosecond)->Repetitions(1)->UseRealTime();
 }
 
-BENCHMARK_TEMPLATE(BM_posdef, Eigen::LLT<Eigen::MatrixXd>)->Apply(custom_args);
-BENCHMARK_TEMPLATE(BM_posdef, Eigen::LDLT<Eigen::MatrixXd>)->Apply(custom_args);
-BENCHMARK_TEMPLATE(BM_posdef, Eigen::BunchKaufman<Eigen::MatrixXd>)
+BENCHMARK_TEMPLATE(BM_pos_def, Eigen::LLT<Eigen::MatrixXd>)->Apply(custom_args);
+BENCHMARK_TEMPLATE(BM_pos_def, Eigen::LDLT<Eigen::MatrixXd>)
     ->Apply(custom_args);
+BENCHMARK_TEMPLATE(BM_pos_def, Eigen::BunchKaufman<Eigen::MatrixXd>)
+    ->Apply(custom_args);
+BENCHMARK_TEMPLATE(BM_pos_sem_def, Eigen::LLT<Eigen::MatrixXd>)
+    ->Apply(custom_args_pos_sem_def);
+BENCHMARK_TEMPLATE(BM_pos_sem_def, Eigen::LDLT<Eigen::MatrixXd>)
+    ->Apply(custom_args_pos_sem_def);
+BENCHMARK_TEMPLATE(BM_pos_sem_def, Eigen::BunchKaufman<Eigen::MatrixXd>)
+    ->Apply(custom_args_pos_sem_def);
 BENCHMARK_TEMPLATE(BM_indefinite, Eigen::LDLT<Eigen::MatrixXd>)
     ->Apply(custom_args);
 BENCHMARK_TEMPLATE(BM_indefinite, Eigen::BunchKaufman<Eigen::MatrixXd>)
