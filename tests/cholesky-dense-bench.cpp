907bc0d0c1262ec183797d7befa56e0af9a523df
Joris Vaillant <joris.vaillant@inria.fr>
2023-12-02 00:22:53 +0100

    [tests] Add a script to plot cholesky dense benchmark

--- tests/cholesky-dense-bench.cpp
+++ tests/cholesky-dense-bench.cpp
@@ -9,6 +9,7 @@
 
 #include <benchmark/benchmark.h>
 
+/// Test decomposition algorithm on dense positive definite matrices
 template <class DecType> void BM_pos_def(benchmark::State &state) {
   Eigen::Rand::P8_mt19937_64 rng{42};
   typedef typename DecType::MatrixType MatrixType;
@@ -25,6 +26,7 @@ template <class DecType> void BM_pos_def(benchmark::State &state) {
   }
 }
 
+/// Test decomposition algorithm on dense positive semi definite matrices
 template <class DecType> void BM_pos_sem_def(benchmark::State &state) {
   Eigen::Rand::P8_mt19937_64 rng{42};
   typedef typename DecType::MatrixType MatrixType;
@@ -42,6 +44,7 @@ template <class DecType> void BM_pos_sem_def(benchmark::State &state) {
   }
 }
 
+/// Test decomposition algorithm on indefinite matrices
 template <class DecType, typename MatrixType = typename DecType::MatrixType>
 void BM_indefinite(benchmark::State &state) {
   Eigen::Rand::P8_mt19937_64 rng{42};
@@ -58,37 +61,35 @@ void BM_indefinite(benchmark::State &state) {
   }
 }
 
-const std::vector<long> dimArgs = {55, 64, 77, 115, 128, 256, 432,
+/// TODO 1000, 2000 and 3000 take really long time
+const std::vector<long> dimArgs = {55, 64,
                                    /*1000, 2000, 4832*/};
-const std::vector<long> nonDefArgs = {1, 2, 4, 8, 16, 32};
+const std::vector<long> nonDefArgs = {1, 32};
 static void custom_args(benchmark::internal::Benchmark *b) {
   b->ArgName("dim");
   for (auto d : dimArgs)
     b->Arg(d);
-  b->Unit(benchmark::kMicrosecond)->Repetitions(1)->UseRealTime();
+  b->Unit(benchmark::kMicrosecond)->UseRealTime()->MinWarmUpTime(0.1);
 }
 static void custom_args_pos_sem_def(benchmark::internal::Benchmark *b) {
-  b->ArgNames({"dim", "non def"});
+  b->ArgNames({"dim", "non_def"});
   b->ArgsProduct({dimArgs, nonDefArgs});
-  b->Unit(benchmark::kMicrosecond)->Repetitions(1)->UseRealTime();
+  b->Unit(benchmark::kMicrosecond)->UseRealTime()->MinWarmUpTime(0.1);
 }
 
-BENCHMARK_TEMPLATE(BM_pos_def, Eigen::LLT<Eigen::MatrixXd>)->Apply(custom_args);
-BENCHMARK_TEMPLATE(BM_pos_def, Eigen::LDLT<Eigen::MatrixXd>)
-    ->Apply(custom_args);
-BENCHMARK_TEMPLATE(BM_pos_def, Eigen::BunchKaufman<Eigen::MatrixXd>)
-    ->Apply(custom_args);
-BENCHMARK_TEMPLATE(BM_pos_sem_def, Eigen::LLT<Eigen::MatrixXd>)
+BENCHMARK(BM_pos_def<Eigen::LLT<Eigen::MatrixXd>>)->Apply(custom_args);
+BENCHMARK(BM_pos_def<Eigen::LDLT<Eigen::MatrixXd>>)->Apply(custom_args);
+BENCHMARK(BM_pos_def<Eigen::BunchKaufman<Eigen::MatrixXd>>)->Apply(custom_args);
+BENCHMARK(BM_pos_sem_def<Eigen::LLT<Eigen::MatrixXd>>)
     ->Apply(custom_args_pos_sem_def);
-BENCHMARK_TEMPLATE(BM_pos_sem_def, Eigen::LDLT<Eigen::MatrixXd>)
+BENCHMARK(BM_pos_sem_def<Eigen::LDLT<Eigen::MatrixXd>>)
     ->Apply(custom_args_pos_sem_def);
-BENCHMARK_TEMPLATE(BM_pos_sem_def, Eigen::BunchKaufman<Eigen::MatrixXd>)
+BENCHMARK(BM_pos_sem_def<Eigen::BunchKaufman<Eigen::MatrixXd>>)
     ->Apply(custom_args_pos_sem_def);
-BENCHMARK_TEMPLATE(BM_indefinite, Eigen::LDLT<Eigen::MatrixXd>)
+BENCHMARK(BM_indefinite<Eigen::LDLT<Eigen::MatrixXd>>)->Apply(custom_args);
+BENCHMARK(BM_indefinite<Eigen::BunchKaufman<Eigen::MatrixXd>>)
     ->Apply(custom_args);
-BENCHMARK_TEMPLATE(BM_indefinite, Eigen::BunchKaufman<Eigen::MatrixXd>)
-    ->Apply(custom_args);
-BENCHMARK_TEMPLATE(BM_indefinite, proxnlp::linalg::DenseLDLT<double>)
+BENCHMARK(BM_indefinite<proxnlp::linalg::DenseLDLT<double>>)
     ->Apply(custom_args);
 
 BENCHMARK_MAIN();
