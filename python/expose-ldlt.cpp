09f3efb305d1ea511bef02fe25a4d0f41b2cd345
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-04 19:58:41 +0100

    [python] expose-ldlt : expose BunchKaufman factorization

--- python/expose-ldlt.cpp
+++ python/expose-ldlt.cpp
@@ -14,11 +14,15 @@ struct LDLTVisitor : bp::def_visitor<LDLTVisitor<LDLTtype>> {
     return fac.compute(mat);
   }
 
+  static bool solveInPlace_proxy(const LDLTtype &fac,
+                                 context::MatrixRef rhsAndX) {
+    return fac.solveInPlace(rhsAndX);
+  }
+
   template <typename... Args> void visit(bp::class_<Args...> &cl) const {
     cl.def("compute", compute_proxy, policies::return_internal_reference,
            bp::args("self", "mat"))
-        .def("solveInPlace", &LDLTtype::solveInPlace,
-             bp::args("self", "rhsAndX"))
+        .def("solveInPlace", solveInPlace_proxy, bp::args("self", "rhsAndX"))
         .def("matrixLDLT", &LDLTtype::matrixLDLT, policies::return_by_value,
              "Get the current value of the decomposition matrix. This makes a "
              "copy.");
@@ -27,9 +31,18 @@ struct LDLTVisitor : bp::def_visitor<LDLTVisitor<LDLTtype>> {
 
 void exposeLdltRoutines() {
   using context::Scalar;
+  using context::VectorXs;
 
   using DenseLDLT = linalg::DenseLDLT<Scalar>;
   bp::class_<DenseLDLT>("DenseLDLT", bp::no_init).def(LDLTVisitor<DenseLDLT>());
+
+  using BunchKaufman_t = Eigen::BunchKaufman<context::MatrixXs, Eigen::Lower>;
+  bp::class_<BunchKaufman_t>("BunchKaufman", bp::no_init)
+      .def(LDLTVisitor<BunchKaufman_t>())
+      .def("pivots", &BunchKaufman_t::pivots, bp::return_internal_reference<>())
+      .def(
+          "subdiag",
+          +[](const BunchKaufman_t &bk) -> VectorXs { return bk.subdiag(); });
 
   using BlockLDLT = linalg::BlockLDLT<Scalar>;
   bp::class_<BlockLDLT>("BlockLDLT", bp::no_init)
