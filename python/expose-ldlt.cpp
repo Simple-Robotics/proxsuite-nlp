d1588e7caab02aa0b5233340af1095e9f2741ae5
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-04 19:31:42 +0100

    [python] expose-ldlt: introduce LDLTVisitor<...>

--- python/expose-ldlt.cpp
+++ python/expose-ldlt.cpp
@@ -6,26 +6,41 @@
 namespace proxnlp {
 namespace python {
 
+template <class LDLTtype>
+struct LDLTVisitor : bp::def_visitor<LDLTVisitor<LDLTtype>> {
+
+  static LDLTtype &compute_proxy(LDLTtype &fac,
+                                 const context::ConstMatrixRef &mat) {
+    return fac.compute(mat);
+  }
+
+  template <typename... Args> void visit(bp::class_<Args...> &cl) const {
+    cl.def("compute", compute_proxy, policies::return_internal_reference,
+           bp::args("self", "mat"))
+        .def("solveInPlace", &LDLTtype::solveInPlace,
+             bp::args("self", "rhsAndX"))
+        .def("matrixLDLT", &LDLTtype::matrixLDLT, policies::return_by_value,
+             "Get the current value of the decomposition matrix. This makes a "
+             "copy.");
+  }
+};
+
 void exposeLdltRoutines() {
   using context::Scalar;
-  using LDLTBase = linalg::ldlt_base<Scalar>;
-  bp::class_<LDLTBase, boost::noncopyable>(
-      "LDLTBase", "Base class for LDLT solvers.", bp::no_init)
-      .def("compute", &LDLTBase::compute, policies::return_internal_reference,
-           bp::args("self", "mat"))
-      .def("solveInPlace", &LDLTBase::solveInPlace, bp::args("self", "rhsAndX"))
-      .def("matrixLDLT", &LDLTBase::matrixLDLT, policies::return_by_value,
-           "Get the current value of the decomposition matrix. This makes a "
-           "copy.");
-  bp::class_<linalg::DenseLDLT<Scalar>, bp::bases<LDLTBase>>("DenseLDLT",
-                                                             bp::no_init);
+
+  using DenseLDLT = linalg::DenseLDLT<Scalar>;
+  bp::class_<DenseLDLT>("DenseLDLT", bp::no_init).def(LDLTVisitor<DenseLDLT>());
+
   using BlockLDLT = linalg::BlockLDLT<Scalar>;
-  bp::class_<BlockLDLT, bp::bases<LDLTBase>>("BlockLDLT", bp::no_init)
+  bp::class_<BlockLDLT>("BlockLDLT", bp::no_init)
+      .def(LDLTVisitor<BlockLDLT>())
       .def("print_sparsity", &BlockLDLT::print_sparsity, bp::args("self"),
            "Print the sparsity pattern of the matrix to factorize.");
 #ifdef PROXNLP_ENABLE_PROXSUITE_LDLT
-  bp::class_<linalg::ProxSuiteLDLTWrapper<Scalar>, bp::bases<LDLTBase>>(
-      "ProxSuiteLDLT", "Wrapper around ProxSuite's custom LDLT.", bp::no_init);
+  using ProxSuiteLDLT = linalg::ProxSuiteLDLTWrapper<Scalar>;
+  bp::class_<ProxSuiteLDLT>(
+      "ProxSuiteLDLT", "Wrapper around ProxSuite's custom LDLT.", bp::no_init)
+      .def(LDLTVisitor<ProxSuiteLDLT>());
 #endif
 }
 
