ab7a240d2cc2dbe1d828bd8209b696ead6272a73
ManifoldFR <wilson.jallet@polytechnique.org>
2023-12-18 17:12:01 +0100

    [cmake] rename target from proxnlp to proxsuite-nlp
    + add ALIAS target proxsuite::nlp
    
    [python] Rename module name from proxnlp to proxsuite_nlp
    
    + [cmake] add compile definition MODULE_NAME="pyproxsuite_nlp"
    + rename directory python/proxnlp to python/proxsuite_nlp
    + update pyproject.toml (notably the ruff exception rules)

--- python/proxsuite_nlp/__init__.py
+++ python/proxsuite_nlp/__init__.py
@@ -1,8 +1,8 @@
 """
 Copyright (C) 2022 LAAS-CNRS, INRIA
 """
-from .pyproxnlp import *
-from .pyproxnlp import __version__
+from .pyproxsuite_nlp import *
+from .pyproxsuite_nlp import __version__
 
 from . import utils
 
@@ -10,15 +10,15 @@ from . import utils
 def __process():
     import sys
     import inspect
-    from . import pyproxnlp
+    from . import pyproxsuite_nlp
 
-    lib_name = "proxnlp"
+    lib_name = "proxsuite_nlp"
 
-    submodules = inspect.getmembers(pyproxnlp, inspect.ismodule)
+    submodules = inspect.getmembers(pyproxsuite_nlp, inspect.ismodule)
     for mod_info in submodules:
         mod_name = "{}.{}".format(lib_name, mod_info[0])
         sys.modules[mod_name] = mod_info[1]
-        mod_info[1].__file__ = pyproxnlp.__file__
+        mod_info[1].__file__ = pyproxsuite_nlp.__file__
         mod_info[1].__name__ = mod_name
 
 
