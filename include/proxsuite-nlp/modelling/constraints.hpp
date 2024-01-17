7970c472ee32fe43c3c57cfd9751580e5df27017
Joris Vaillant <joris.vaillant@inria.fr>
2024-01-17 10:37:32 +0100

    [core] Update constraints.hpp

--- include/proxsuite-nlp/modelling/constraints.hpp
+++ include/proxsuite-nlp/modelling/constraints.hpp
@@ -6,19 +6,5 @@
 #include "proxsuite-nlp/modelling/constraints/l1-penalty.hpp"
 
 #ifdef PROXSUITE_NLP_ENABLE_TEMPLATE_INSTANTIATION
-#include "proxsuite-nlp/context.hpp"
 #include "proxsuite-nlp/modelling/constraints.txx"
-
-namespace proxsuite {
-namespace nlp {
-
-extern template struct ConstraintSetBase<context::Scalar>;
-extern template struct ConstraintObjectTpl<context::Scalar>;
-extern template struct EqualityConstraint<context::Scalar>;
-extern template struct NegativeOrthant<context::Scalar>;
-extern template struct BoxConstraintTpl<context::Scalar>;
-extern template struct NonsmoothPenaltyL1Tpl<context::Scalar>;
-
-} // namespace nlp
-} // namespace proxsuite
 #endif
