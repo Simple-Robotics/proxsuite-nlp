74ef860f7bbc071ee956ab129ffb767fce5c9bb8
Joris Vaillant <joris.vaillant@inria.fr>
2024-01-17 11:20:08 +0100

    [python] Add windows_dll_manager module to manage add_dll_directory on Windows

--- bindings/python/proxsuite_nlp/__init__.py
+++ bindings/python/proxsuite_nlp/__init__.py
@@ -1,8 +1,32 @@
 """
 Copyright (C) 2022 LAAS-CNRS, INRIA
 """
-from .pyproxsuite_nlp import *
-from .pyproxsuite_nlp import __version__
+
+# On Windows, if proxsuite-nlp.dll is not in the same directory than
+# the .pyd, it will not be loaded.
+# We first try to load proxsuite-nlp, then, if it fail and we are on Windows:
+#  1. We add all paths inside proxsuite-nlp_WINDOWS_DLL_PATH to DllDirectory
+#  2. If proxsuite-nlp_WINDOWS_DLL_PATH we add the relative path from the
+#     package directory to the bin directory to DllDirectory
+# This solution is inspired from:
+#  - https://github.com/PixarAnimationStudios/OpenUSD/pull/1511/files
+#  - https://stackoverflow.com/questions/65334494/python-c-extension-packaging-dll-along-with-pyd
+# More resources on https://github.com/diffpy/pyobjcryst/issues/33
+try:
+    from .pyproxsuite_nlp import *
+    from .pyproxsuite_nlp import __version__
+except ImportError:
+    import platform
+    if platform.system() == "Windows":
+        from .windows_dll_manager import get_dll_paths, build_directory_manager
+
+        with build_directory_manager() as dll_dir_manager:
+            for p in get_dll_paths():
+                dll_dir_manager.add_dll_directory(p)
+            from .pinocchio_pywrap import *
+            from .pinocchio_pywrap import __version__, __raw_version__
+    else:
+        raise
 
 from . import utils
 
