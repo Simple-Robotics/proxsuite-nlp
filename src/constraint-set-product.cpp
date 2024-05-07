114093d8f2bb27bdf51f94be34ae619a88642915
ManifoldFR <wilson.jallet@polytechnique.org>
2024-05-07 23:20:10 +0200

    [modelling] add template instantiation for ConstraintSetProductTpl

---
+++ src/constraint-set-product.cpp
@@ -0,0 +1,7 @@
+#include "proxsuite-nlp/modelling/constraints/constraint-set-product.hpp"
+
+namespace proxsuite {
+namespace nlp {
+template struct ConstraintSetProductTpl<context::Scalar>;
+} // namespace nlp
+} // namespace proxsuite
