e74315eeaeaf03746cbce5deb7365030a5bb5e7b
ManifoldFR <wilson.jallet@polytechnique.org>
2024-05-29 00:31:08 +0200

    [tests/python] test passthrough function taking polymorphic value type

--- tests/python/test_polymorphic.py
+++ tests/python/test_polymorphic.py
@@ -1,4 +1,13 @@
-from polymorphic_test import X, Y, Z, getY, getZ, poly_use_base, create_vec_poly
+from polymorphic_test import (
+    X,
+    Y,
+    Z,
+    getY,
+    getZ,
+    poly_use_base,
+    create_vec_poly,
+    poly_passthrough,
+)
 
 assert isinstance(getY(), Y)
 assert isinstance(getZ(), Z)
@@ -6,7 +15,9 @@ assert issubclass(Y, X)
 assert issubclass(Z, X)
 
 y = Y()
+print(y)
 z = Z()
+print(z)
 
 b = poly_use_base(y)
 print(b.x)
@@ -17,6 +28,14 @@ print(b.x)
 assert isinstance(b.x, Z)
 
 vec = create_vec_poly()
-print(len(vec))
+assert len(vec) == 3
 print(vec[0])
 print(vec.tolist())
+
+print("passthrough:")
+py = poly_passthrough(y)
+assert isinstance(py, Y)
+print(py)
+pz = poly_passthrough(z)
+assert isinstance(pz, Z)
+print(pz)
