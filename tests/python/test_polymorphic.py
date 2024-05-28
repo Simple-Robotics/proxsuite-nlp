a41c85cb3fa3637470044ad8edd83df4322418e3
ManifoldFR <wilson.jallet@polytechnique.org>
2024-05-28 23:31:51 +0200

    [tests/python] test create a vector of polymorphic

--- tests/python/test_polymorphic.py
+++ tests/python/test_polymorphic.py
@@ -1,4 +1,4 @@
-from polymorphic_test import X, Y, Z, getY, getZ, poly_use_base
+from polymorphic_test import X, Y, Z, getY, getZ, poly_use_base, create_vec_poly
 
 assert isinstance(getY(), Y)
 assert isinstance(getZ(), Z)
@@ -15,3 +15,8 @@ assert isinstance(b.x, Y)
 b = poly_use_base(z)
 print(b.x)
 assert isinstance(b.x, Z)
+
+vec = create_vec_poly()
+print(len(vec))
+print(vec[0])
+print(vec.tolist())
