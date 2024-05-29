9e55851ccbe8e55a00844212e44fc4c619d1d71a
ManifoldFR <wilson.jallet@polytechnique.org>
2024-05-29 14:22:10 +0200

    [python] Update expose-residual.cpp

--- bindings/python/expose-residual.cpp
+++ bindings/python/expose-residual.cpp
@@ -30,14 +30,14 @@ void exposeResiduals() {
   expose_function<ManifoldDifferenceToPoint<Scalar>>(
       "ManifoldDifferenceToPoint", "Difference vector x (-) x0.",
       bp::init<const polymorphic<Manifold> &, const ConstVectorRef &>(
-          bp::args("self", "space", "target")))
+          ("self"_a, "space", "target")))
       .add_property("target", &ManifoldDifferenceToPoint<Scalar>::target_);
 
   expose_function<LinearFunctionDifferenceToPoint<Scalar>>(
       "LinearFunctionDifferenceToPoint",
       "Linear function of the vector difference to a reference point.",
       bp::init<const polymorphic<Manifold> &, VectorXs, MatrixXs, VectorXs>(
-          bp::args("self", "space", "target", "A", "b")));
+          ("self"_a, "space", "target", "A", "b")));
 }
 
 } // namespace python
