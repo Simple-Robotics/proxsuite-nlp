86584c93dcaad889305da88d0e855bf8528d6c96
ManifoldFR <wilson.jallet@polytechnique.org>
2024-05-29 17:12:17 +0200

    [tests/python] polymorphic: test for type Z inheriting from Y

--- tests/python/test_polymorphic.py
+++ tests/python/test_polymorphic.py
@@ -1,33 +1,57 @@
 from polymorphic_test import (
     X,
     Y,
-    Z,
+    W,
     getY,
-    getZ,
+    getW,
     poly_use_base,
+    poly_store,
     create_vec_poly,
+    set_return,
     poly_passthrough,
+    call_method,
     get_const_y_poly_ref,
 )
 
 assert isinstance(getY(), Y)
-assert isinstance(getZ(), Z)
+assert isinstance(getW(), W)
 assert issubclass(Y, X)
-assert issubclass(Z, X)
+assert issubclass(W, Y)
 
 y = Y()
 print(y)
-z = Z()
-print(z)
+call_method(y)
+w = W()
+print(w)
 
-b = poly_use_base(y)
-print(b.x)
-assert isinstance(b.x, Y)
 
-b = poly_use_base(z)
-print(b.x)
-assert isinstance(b.x, Z)
+def test_poly_base():
+    print("Use base")
+    b = poly_use_base(y)
+    print(b.x)
+    assert isinstance(b.x, Y)
 
+    b = poly_use_base(w)
+    print(b.x)
+    assert isinstance(b.x, W)
+
+
+def test_poly_store():
+    print("test_poly_store")
+    b = poly_store(y)
+    print(b.x)
+    assert isinstance(b.x, Y)
+
+    b = poly_store(w)
+    # wrong, we get Y
+    print(b.x)
+    assert isinstance(b.x, W)
+
+
+test_poly_base()
+test_poly_store()
+
+print("Create vector<PolyX>")
 vec = create_vec_poly()
 assert len(vec) == 3
 print(vec[0])
@@ -36,12 +60,19 @@ print(vec.tolist())
 print("passthrough:")
 py = poly_passthrough(y)
 assert isinstance(py, Y)
-print(py)
-pz = poly_passthrough(z)
-assert isinstance(pz, Z)
-print(pz)
+print("y", py)
+
+pw = poly_passthrough(w)
+print("w", pw)
+assert isinstance(pw, W)  # error
+assert isinstance(pw, Y)
 
 print("Get static Y:")
 ry_static = get_const_y_poly_ref()
 print(ry_static)
 assert isinstance(ry_static, Y)
+
+print("Set static and return:")
+r_stat = set_return(w)
+print("r_stat (W):", r_stat)
+assert isinstance(r_stat, W)
