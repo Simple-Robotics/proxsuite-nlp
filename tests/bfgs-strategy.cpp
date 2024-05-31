4b09385049b86874891b794eddbe7882b93b3d0a
Fabian Schramm <55981657+fabinsch@users.noreply.github.com>
2024-05-31 15:08:31 +0200

    add unittests for BfgsStrategy

---
+++ tests/bfgs-strategy.cpp
@@ -0,0 +1,134 @@
+#include <boost/test/unit_test.hpp>
+#include "util.hpp"
+
+#include "proxsuite-nlp/bfgs-strategy.hpp"
+
+using namespace proxsuite::nlp;
+
+BOOST_AUTO_TEST_CASE(test_inverse_hessian_update) {
+  const long nx = 4;
+  using BfgsStrategy_t = BfgsStrategy<Scalar>; // default to InverseHessian
+  BfgsStrategy_t bfgs(nx);
+  VectorXs x0 = VectorXs::Random(nx);
+  VectorXs g0 = VectorXs::Random(nx);
+  bfgs.init(x0, g0);
+
+  VectorXs x;
+  VectorXs g;
+  VectorXs s;
+  VectorXs y;
+
+  bool is_psd = false;
+  while (!is_psd) {
+    x = VectorXs::Random(nx);
+    g = VectorXs::Random(nx);
+    s = x - x0;
+    y = g - g0;
+    if (s.dot(y) > 0) {
+      bfgs.update(x, g);
+      is_psd = bfgs.is_psd;
+    }
+  }
+
+  Scalar rho = 1. / s.dot(y);
+  MatrixXs H_inv_prev = MatrixXs::Identity(nx, nx);
+  MatrixXs H_inv = (MatrixXs::Identity(nx, nx) - rho * s * y.transpose()) *
+                       H_inv_prev *
+                       (MatrixXs::Identity(nx, nx) - rho * y * s.transpose()) +
+                   rho * s * s.transpose();
+  BOOST_TEST_CHECK(bfgs.M.isApprox(H_inv, 1e-6));
+}
+
+BOOST_AUTO_TEST_CASE(test_hessian_update) {
+  const long nx = 4;
+  using BfgsStrategy_t = BfgsStrategy<Scalar, BfgsType::Hessian>;
+  BfgsStrategy_t bfgs(nx);
+  VectorXs x0 = VectorXs::Random(nx);
+  VectorXs g0 = VectorXs::Random(nx);
+  bfgs.init(x0, g0);
+
+  VectorXs x;
+  VectorXs g;
+  VectorXs s;
+  VectorXs y;
+
+  bool is_psd = false;
+  while (!is_psd) {
+    x = VectorXs::Random(nx);
+    g = VectorXs::Random(nx);
+    s = x - x0;
+    y = g - g0;
+    if (s.dot(y) > 0) {
+      bfgs.update(x, g);
+      is_psd = bfgs.is_psd;
+    }
+  }
+
+  Scalar rho = 1. / s.dot(y);
+  MatrixXs H_prev = MatrixXs::Identity(nx, nx);
+  MatrixXs H = (MatrixXs::Identity(nx, nx) - rho * y * s.transpose()) * H_prev *
+                   (MatrixXs::Identity(nx, nx) - rho * s * y.transpose()) +
+               rho * y * y.transpose();
+  BOOST_TEST_CHECK(bfgs.M.isApprox(H, 1e-6));
+}
+
+BOOST_AUTO_TEST_CASE(test_bfgs_inverse_hessian) {
+  const long nx = 4;
+  using BfgsStrategy_t = BfgsStrategy<double, BfgsType::InverseHessian>;
+  BfgsStrategy_t bfgs(nx);
+
+  // random quadratic function with positive definite Hessian
+  Eigen::MatrixXd H = Eigen::MatrixXd::Random(nx, nx);
+  H = H.transpose() * H; // make it symmetric and positive definite
+  Eigen::MatrixXd H_inv = H.inverse();
+  Eigen::VectorXd b = Eigen::VectorXd::Random(nx);
+
+  Eigen::VectorXd x0 = Eigen::VectorXd::Random(nx);
+  Eigen::VectorXd g0 = H * x0 - b;
+  bfgs.init(x0, g0);
+
+  for (int i = 0; i < 1000; ++i) {
+    Eigen::VectorXd x = Eigen::VectorXd::Random(nx);
+    Eigen::VectorXd g = H * x - b; // gradient
+    bfgs.update(x, g);
+
+    Eigen::MatrixXd H_inv_approx = bfgs.M;
+    Eigen::MatrixXd H_inv_err = H_inv - H_inv_approx;
+    double err = H_inv_err.norm();
+    if (err < 1e-5) {
+      break;
+    }
+  }
+
+  BOOST_TEST_CHECK(bfgs.M.isApprox(H_inv, 1e-5));
+}
+
+BOOST_AUTO_TEST_CASE(test_bfgs_hessian) {
+  const long nx = 4;
+  using BfgsStrategy_t = BfgsStrategy<double, BfgsType::Hessian>;
+  BfgsStrategy_t bfgs(nx);
+
+  // random quadratic function with positive definite Hessian
+  Eigen::MatrixXd H = Eigen::MatrixXd::Random(nx, nx);
+  H = H.transpose() * H; // make it symmetric and positive definite
+  Eigen::VectorXd b = Eigen::VectorXd::Random(nx);
+
+  Eigen::VectorXd x0 = Eigen::VectorXd::Random(nx);
+  Eigen::VectorXd g0 = H * x0 - b;
+  bfgs.init(x0, g0);
+
+  for (int i = 0; i < 1000; ++i) {
+    Eigen::VectorXd x = Eigen::VectorXd::Random(nx);
+    Eigen::VectorXd g = H * x - b; // gradient
+    bfgs.update(x, g);
+
+    Eigen::MatrixXd H_approx = bfgs.M;
+    Eigen::MatrixXd H_err = H - H_approx;
+    double err = H_err.norm();
+    if (err < 1e-5) {
+      break;
+    }
+  }
+
+  BOOST_TEST_CHECK(bfgs.M.isApprox(H, 1e-5));
+}
